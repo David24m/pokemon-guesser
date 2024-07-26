beb2bd5ad00f4b40629b20a42c0b6f8903c546e6
Kirsty Love <kirstylove@Kirstys-MacBook-Pro.local>
2024-07-26 16:42:20 +0100

    add pokemonRouter to get whole list for now

--- source/routes/pokemon.router.ts
+++ source/routes/pokemon.router.ts
@@ -1,8 +1,24 @@
-import express from 'express';
-import controller from '../controllers/test.controller';
+import express, { NextFunction } from 'express';
+const logger = require('./logger');
+import { PokemonService } from '../services/pokemon.service';
 
 const pokemonRouter = express.Router();
 
-pokemonRouter.get('/pokemonList', controller.getPokemonFunction);
+const pokemonService = new PokemonService();
+
+pokemonRouter.get('/pokemonList', ( async (
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+        logger.info('recieved request for pokemon list');
+        try {
+            const result = pokemonService.getPokemonList()
+
+            return result;
+        } catch (error) {
+            return next(error);
+        }
+    })
+);
 
 export default pokemonRouter;
