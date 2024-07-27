a62dba61a576d3f41b645aaf8a78cb36775e1258
Kirsty Love <kirstylove@Kirstys-MacBook-Pro.local>
2024-07-27 21:21:42 +0100

    add swagger file

--- source/controllers/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import axios, { AxiosResponse } from "axios";
-
-const getPokemonFunction = async (req: Request, res: Response, next: NextFunction) => {
-    let result: AxiosResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-shape/1/`)
-    return res.send(result.data);
-};
-
-export default { getPokemonFunction }
