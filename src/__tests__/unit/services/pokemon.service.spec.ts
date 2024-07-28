16281709e0061ff321ee9e10f86a00702698b9ee
Kirsty Love <kirstylove@Kirstys-MacBook-Pro.local>
2024-07-28 21:28:36 +0100

    tidy up the tests

--- src/__tests__/unit/services/pokemon.service.spec.ts
+++ src/__tests__/unit/services/pokemon.service.spec.ts
@@ -1,56 +1,58 @@
 import { PokemonService } from "../../../services/pokemon.service"
 import { pokemonListResponse } from "../../../__mocks__/pokemon.lis.response";
 
-describe('getPokemonList', () => {
-    it('should call getPokemonList correctly', async () => {
-        const pokemonService = new PokemonService();
+describe('pokemon service test', () => {
+    describe('getPokemonList', () => {
+        it('should call getPokemonList correctly', async () => {
+            const pokemonService = new PokemonService();
 
-        const result = await pokemonService.getPokemonList()
+            const result = await pokemonService.getPokemonList()
 
-        expect(result).toBeDefined()
+            expect(result).toBeDefined()
+        })
     })
-})
 
-describe('shuffledPokemon', () => {
-    it('should shuffle list with shuffledPokemon correctly', async () => {
-        const pokemonService = new PokemonService();
+    describe('shuffledPokemon', () => {
+        it('should shuffle list with shuffledPokemon correctly', async () => {
+            const pokemonService = new PokemonService();
 
-        const result = pokemonService.shuffledPokemon(pokemonListResponse)
+            const result = pokemonService.shuffledPokemon(pokemonListResponse)
 
-        expect(result).toBeDefined()
-        expect(result).toEqual(expect.arrayContaining([{ name: 'pikachu', url: 'https://pokeapi.co/api/v2/pokemon/25/' }]))
+            expect(result).toBeDefined()
+            expect(result).toEqual(expect.arrayContaining([{ name: 'pikachu', url: 'https://pokeapi.co/api/v2/pokemon/25/' }]))
+        })
     })
-})
 
-describe('getFourRandomPokemon', () => {
-    it('should call getFourRandomPokemon correctly', async () => {
-        const pokemonService = new PokemonService();
+    describe('getFourRandomPokemon', () => {
+        it('should call getFourRandomPokemon correctly', async () => {
+            const pokemonService = new PokemonService();
 
-        const result = pokemonService.getFourRandomPokemon(pokemonListResponse)
+            const result = pokemonService.getFourRandomPokemon(pokemonListResponse)
 
-        expect(result).toBeDefined();
-        expect(result).toHaveLength(4);
+            expect(result).toBeDefined();
+            expect(result).toHaveLength(4);
+        })
     })
-})
 
-describe('getPokemonPicture', () => {
-    it('should call getPokemonPicture correctly', async () => {
-        const pokemonService = new PokemonService();
+    describe('getPokemonPicture', () => {
+        it('should call getPokemonPicture correctly', async () => {
+            const pokemonService = new PokemonService();
 
-        const result = pokemonService.getPokemonPicture({ name: 'koffing', url: 'https://pokeapi.co/api/v2/pokemon/109/' })
+            const result = pokemonService.getPokemonPicture({ name: 'koffing', url: 'https://pokeapi.co/api/v2/pokemon/109/' })
 
-        expect(result).toBeDefined();
-        expect(result).toContain('109')
+            expect(result).toBeDefined();
+            expect(result).toContain('109')
+        })
     })
-})
 
-describe('getPokemonNumber', () => {
-    it('should call getPokemonNumber correctly', async () => {
-        const pokemonService = new PokemonService();
+    describe('getPokemonNumber', () => {
+        it('should call getPokemonNumber correctly', async () => {
+            const pokemonService = new PokemonService();
 
-        const result = pokemonService.getPokemonNumber(`https://pokeapi.co/api/v2/pokemon/109/`)
+            const result = pokemonService.getPokemonNumber(`https://pokeapi.co/api/v2/pokemon/109/`)
 
-        expect(result).toBeDefined();
-        expect(result).toEqual('109')
+            expect(result).toBeDefined();
+            expect(result).toEqual('109')
+        })
     })
 })
