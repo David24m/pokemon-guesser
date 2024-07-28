ec496cd222d3f4c8d707bf6901801c95e1c8b6ca
Kirsty Love <kirstylove@Kirstys-MacBook-Pro.local>
2024-07-28 18:23:19 +0100

    add tests for services

--- src/__tests__/unit/services/pokemon.service.spec.ts
+++ src/__tests__/unit/services/pokemon.service.spec.ts
@@ -1,13 +1,58 @@
 import { PokemonService } from "../../../services/pokemon.service"
+import { pokemonListResponse } from "../../../__mocks__/pokemon.lis.response";
 
 describe('getPokemonList', () => {
     it('should call getPokemonList correctly', async () => {
         const pokemonService = new PokemonService();
 
-        const result = pokemonService.getPokemonList()
-
-        console.log('result:', result)
+        const result = await pokemonService.getPokemonList()
 
         expect(result).toBeDefined()
+    })
+})
+
+describe('shuffledPokemon', () => {
+    it('should shuffle list with shuffledPokemon correctly', async () => {
+        const pokemonService = new PokemonService();
+
+        const result = pokemonService.shuffledPokemon(pokemonListResponse)
+
+        expect(result).toBeDefined()
+        expect(result).toEqual(expect.arrayContaining([{ name: 'pikachu', url: 'https://pokeapi.co/api/v2/pokemon/25/' }]))
+    })
+})
+
+describe('getFourRandomPokemon', () => {
+    it('should call getFourRandomPokemon correctly', async () => {
+        const pokemonService = new PokemonService();
+
+        const result = pokemonService.getFourRandomPokemon(pokemonListResponse)
+
+        expect(result).toBeDefined();
+        expect(result).toHaveLength(4);
+    })
+})
+
+describe('getPokemonPicture', () => {
+    it('should call getPokemonPicture correctly', async () => {
+        const pokemonService = new PokemonService();
+
+        const result = pokemonService.getPokemonPicture({ name: 'koffing', url: 'https://pokeapi.co/api/v2/pokemon/109/' })
+
+        expect(result).toBeDefined();
+        expect(result).toContain('109')
+    })
+})
+
+describe('getPokemonNumber', () => {
+    it('should call getPokemonNumber correctly', async () => {
+        const pokemonService = new PokemonService();
+
+        const result = pokemonService.getPokemonNumber(`https://pokeapi.co/api/v2/pokemon/109/`)
+
+        console.log(result);
+
+        expect(result).toBeDefined();
+        expect(result).toEqual('109')
     })
 })
